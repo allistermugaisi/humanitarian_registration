0dc0746cffdce73462c1696e9d14b5fbe6b6782e
allistermugaisi <allistermugaisi@gmail.com>
2023-02-03 13:40:36 +0300

    Added country

--- js/email.js
+++ js/email.js
@@ -3,16 +3,28 @@
 	emailjs.init('KL07k3FfR0UOyuxdi');
 })();
 
-function sendmail() {
+let sendMailForm = document.getElementById('sendMailForm');
+
+if (sendMailForm.attachEvent) {
+	sendMailForm.attachEvent('submit', sendmail);
+} else {
+	sendMailForm.addEventListener('submit', sendmail);
+}
+
+function sendmail(e) {
+	e.preventDefault();
+
 	let firstName = document.getElementById('first_name').value;
 	let lastName = document.getElementById('last_name').value;
-	let userPhone = document.getElementById('phone').value;
-	let userEmail = document.getElementById('email').value;
+	let userPhone = document.getElementById('from_phone').value;
+	let userEmail = document.getElementById('from_email').value;
 	let businessOrganization = document.getElementById(
 		'business_organization'
 	).value;
+	let courseCategory = document.getElementById('course_category').value;
 	let courseName = document.getElementById('course').value;
-	// let userMessage = document.getElementById('message').value;
+	let country = document.getElementById('country').value;
+	let courseCV = document.getElementById('course_cv').files[0];
 
 	const serviceID = 'service_0ppa9xp';
 	const templateID = 'template_0g7zfdn';
@@ -24,16 +36,20 @@ function sendmail() {
 		from_email: userEmail,
 		business_organization: businessOrganization,
 		course: courseName,
+		course_category: courseCategory,
+		country: country,
+		course_cv: courseCV,
 		reply_to: userEmail,
-		// message: userMessage,
 	};
 	console.log(contactParams);
+	console.log(this);
 
 	alert('Your message has been sent successfully');
 
-	emailjs.send(serviceID, templateID, contactParams).then(
-		function () {
+	emailjs.sendForm(serviceID, templateID, this).then(
+		function (res) {
 			console.log('SUCCESS!');
+			console.log(res);
 		},
 		function (error) {
 			console.log('FAILED...', error);
