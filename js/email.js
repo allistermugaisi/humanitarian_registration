38725a798bf94d0d0a0c2c72e9c2dffd5406b111
allistermugaisi <allistermugaisi@gmail.com>
2023-02-03 11:47:04 +0300

    Connected email js to send emails

--- js/email.js
+++ js/email.js
@@ -1,24 +1,42 @@
 /* Sending Email from Contact Section */
 (function () {
-	emailjs.init('DQNy5qMGUmVTsKugK');
+	emailjs.init('KL07k3FfR0UOyuxdi');
 })();
 
 function sendmail() {
-	let fullName = document.getElementById('name').value;
+	let firstName = document.getElementById('first_name').value;
+	let lastName = document.getElementById('last_name').value;
 	let userPhone = document.getElementById('phone').value;
 	let userEmail = document.getElementById('email').value;
-	let userMessage = document.getElementById('message').value;
+	let businessOrganization = document.getElementById(
+		'business_organization'
+	).value;
+	let courseName = document.getElementById('course').value;
+	// let userMessage = document.getElementById('message').value;
+
+	const serviceID = 'service_0ppa9xp';
+	const templateID = 'template_0g7zfdn';
 
 	let contactParams = {
-		from_name: fullName,
+		to_name: 'Humanitarian Studies',
+		from_name: `${firstName} ${lastName}`,
 		from_phone: userPhone,
 		from_email: userEmail,
-		message: userMessage,
+		business_organization: businessOrganization,
+		course: courseName,
+		reply_to: userEmail,
+		// message: userMessage,
 	};
+	console.log(contactParams);
 
 	alert('Your message has been sent successfully');
 
-	emailjs
-		.send('service_2p0g1ch', 'template_f5fvqfv', contactParams)
-		.then(function (res) {});
+	emailjs.send(serviceID, templateID, contactParams).then(
+		function () {
+			console.log('SUCCESS!');
+		},
+		function (error) {
+			console.log('FAILED...', error);
+		}
+	);
 }
